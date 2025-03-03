3bf8fedb51268af69a243086ffbbf6118f7c8202
oura takeshi <laughseed@gmail.com>
2025-03-03 12:26:59 +0900

    商品一覧画面の追加

---
+++ src/app/Http/Controllers/ProductController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use App\Models\Season;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $products = Product::Paginate(6);
+        return view('index', compact('products'));
+    }
+}
